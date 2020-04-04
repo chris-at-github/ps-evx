9bf29c5ec4c9f2960f4a41ad3d52e65a2efa04b8
Christian Pschorr <c.pschorr@datamints.com>
2020-04-04 23:42:05 +0200

    Tabellenstruktur und TCA für GameMapTile erstellt

--- ext_tables.sql
+++ ext_tables.sql
@@ -26,5 +26,16 @@ CREATE TABLE tx_evx_domain_model_game (
 
 	title varchar(255) DEFAULT '' NOT NULL,
 	map int(11) unsigned DEFAULT '0',
+	tiles int(11) unsigned DEFAULT '0',
+
+);
+
+#
+# Table structure for table 'tx_evx_domain_model_gamemaptile'
+#
+CREATE TABLE tx_evx_domain_model_gamemaptile (
+
+	origin int(11) unsigned DEFAULT '0',
+	game int(11) unsigned DEFAULT '0',
 
 );
