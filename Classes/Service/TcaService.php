da99c951a8d828617a143f23235eff2e01fc9099
Christian Pschorr <c.pschorr@datamints.com>
2020-04-09 00:14:59 +0200

    Ausgabe der X und Y Koordinate von Tiles übergreifend im Backend

--- Classes/Service/TcaService.php
+++ Classes/Service/TcaService.php
@@ -12,6 +12,7 @@ class TcaService {
 	 * @param $parentObject
 	 */
 	public function getTileTitle(&$parameter) {
-		$parameter['title'] = $parameter['row']['x'] . ' | ' . $parameter['row']['y'];
+		$record = BackendUtility::getRecord($parameter['table'], (int) $parameter['row']['uid']);
+		$parameter['title'] = $record['x'] . ' | ' . $record['y'];
 	}
 }
